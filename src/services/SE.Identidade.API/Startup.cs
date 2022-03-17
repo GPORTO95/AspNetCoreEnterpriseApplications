e997b0789fdd4867b832c2d39b7b353e7ab17efc
Gabriel Porto <gr_porto@hotmail.com>
2022-03-16 23:06:26 -0300

    Adicionado classe startup para organização do projeto

---
+++ src/services/SE.Identidade.API/Startup.cs
@@ -0,0 +1,63 @@
+﻿namespace SE.Identidade.API
+{
+    public class Startup : IStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+
+            services.AddControllers();
+            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(WebApplication app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+    }
+
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TSartup>(this WebApplicationBuilder WebAppBuilder) where TSartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TSartup), WebAppBuilder.Configuration) as IStartup;
+            if (startup == null) throw new ArgumentException("Classe Startup.cs inválida!");
+
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+        }
+    }
+}
---
+++ src/web/SE.WebApp.MVC/Startup.cs
@@ -0,0 +1,65 @@
+﻿namespace SE.WebApp.MVC
+{
+    public class Startup : IStartup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add services to the container.
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(WebApplication app, IWebHostEnvironment environment)
+        {
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+        }
+    }
+
+    public interface IStartup
+    {
+        IConfiguration Configuration { get; }
+        void Configure(WebApplication app, IWebHostEnvironment environment);
+        void ConfigureServices(IServiceCollection services);
+    }
+
+    public static class StartupExtensions
+    {
+        public static WebApplicationBuilder UseStartup<TSartup>(this WebApplicationBuilder WebAppBuilder) where TSartup : IStartup
+        {
+            var startup = Activator.CreateInstance(typeof(TSartup), WebAppBuilder.Configuration) as IStartup;
+            if (startup == null) throw new ArgumentException("Classe Startup.cs inválida!");
+
+            startup.ConfigureServices(WebAppBuilder.Services);
+
+            var app = WebAppBuilder.Build();
+            startup.Configure(app, app.Environment);
+
+            app.Run();
+
+            return WebAppBuilder;
+        }
+    }
+}
