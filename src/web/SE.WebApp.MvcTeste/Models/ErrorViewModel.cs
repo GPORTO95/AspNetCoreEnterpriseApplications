b2555e40b07e1835d2d8d0b1fa95861d19de88a3
Gabriel Porto <gr_porto@hotmail.com>
2022-03-26 00:49:22 -0300

    chore: adicionado web mvc de teste para adição de paginas do identity

---
+++ src/web/SE.WebApp.MvcTeste/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SE.WebApp.MvcTeste.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
