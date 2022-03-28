727480a372c5cb0599ae46e173e2b8d550aa3484
Gabriel Porto <gr_porto@hotmail.com>
2022-03-27 23:47:46 -0300

    chore: Adicionado model, views e controller de login

--- src/web/SE.WebApp.MVC/Controllers/IdentidadeController.cs
+++ src/web/SE.WebApp.MVC/Controllers/IdentidadeController.cs
@@ -1,6 +1,57 @@
-﻿namespace SE.WebApp.MVC.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using SE.WebApp.MVC.Models;
+
+namespace SE.WebApp.MVC.Controllers
 {
-    public class IdentidadeController
+    public class IdentidadeController : Controller
     {
+        [HttpGet]
+        [Route("nova-conta")]
+        public IActionResult Registro()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("nova-conta")]
+        public async Task<IActionResult> Registro(UsuarioRegistro usuarioRegistro)
+        {
+            if(!ModelState.IsValid) return View(usuarioRegistro);
+
+            // API - REGISTRO
+
+            // Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("login")]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(UsuarioLogin usuarioLogin)
+        {
+            if (!ModelState.IsValid) return View(usuarioLogin);
+
+            // API - Login
+
+            // Realizar login na APP
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        [HttpGet]
+        [Route("sair")]
+        public async Task<IActionResult> Logout()
+        {
+            // Limpar o cookie da APP
+
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
