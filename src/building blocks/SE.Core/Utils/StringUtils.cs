fc53988a0269faca7488d732c64ac37403fd0b15
Gabriel Porto <gr_porto@hotmail.com>
2022-04-06 23:11:45 -0300

    chore: Adicionado modelagem na api de cliente, objeto de valor e validação

---
+++ src/building blocks/SE.Core/Utils/StringUtils.cs
@@ -0,0 +1,10 @@
+﻿namespace SE.Core.Utils
+{
+    public static class StringUtils
+    {
+        public static string ApenasNumeros(this string str, string input)
+        {
+            return new string(input.Where(char.IsDigit).ToArray());
+        }
+    }
+}
