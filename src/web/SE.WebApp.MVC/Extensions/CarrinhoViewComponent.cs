861525d1b016c94c96f032a1d18fac5fa276be57
Gabriel Porto <gr_porto@hotmail.com>
2022-05-09 22:38:50 -0300

    chore: implementando o carrinho no front end pt - III

---
+++ src/web/SE.WebApp.MVC/Extensions/CarrinhoViewComponent.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SE.WebApp.MVC.Models;
+using SE.WebApp.MVC.Services;
+
+namespace SE.WebApp.MVC.Extensions
+{
+    public class CarrinhoViewComponent : ViewComponent
+    {
+        private readonly ICarrinhoService _carrinhoService;
+
+        public CarrinhoViewComponent(ICarrinhoService carrinhoService)
+        {
+            _carrinhoService = carrinhoService;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View(await _carrinhoService.ObterCarrinho() ?? new CarrinhoViewModel());
+        }
+    }
+}
