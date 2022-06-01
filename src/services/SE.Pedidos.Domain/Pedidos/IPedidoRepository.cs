9a00524df105d58dc5d58e6dc991f4241c9556ab
Gabriel Porto <gr_porto@hotmail.com>
2022-05-31 21:51:50 -0300

    feat: modelagem do pedido no dominio

---
+++ src/services/SE.Pedidos.Domain/Pedidos/IPedidoRepository.cs
@@ -0,0 +1,16 @@
+﻿using SE.Core.Data;
+
+namespace SE.Pedidos.Domain.Pedidos
+{
+    public interface IPedidoRepository : IRepository<Pedido>
+    {
+        Task<Pedido> ObterPorId(Guid id);
+        Task<IEnumerable<Pedido>> ObterListaPorClienteId(Guid clienteId);
+        void Adicionar(Pedido pedido);
+        void Atualizar(Pedido pedido);
+
+        /* Pedido Item */
+        Task<PedidoItem> ObterItemPorId(Guid id);
+        Task<PedidoItem> ObterItemPorPedido(Guid pedidoId, Guid produtoId);
+    }
+}
---
+++ src/services/SE.Pedidos.Domain/Pedidos/PedidoItem.cs
@@ -0,0 +1,33 @@
+﻿using SE.Core.DomainObjects;
+
+namespace SE.Pedidos.Domain.Pedidos
+{
+    public class PedidoItem : Entity
+    {
+        // Ef ctor
+        protected PedidoItem() { }
+
+        public PedidoItem(Guid pedidoId, Guid produtoId, string produtoNome, int quantidaade, decimal valorUnitario, string produtoImagem = null)
+        {
+            PedidoId = pedidoId;
+            ProdutoId = produtoId;
+            ProdutoNome = produtoNome;
+            Quantidaade = quantidaade;
+            ValorUnitario = valorUnitario;
+            ProdutoImagem = produtoImagem;
+        }
+
+        public Guid PedidoId { get; private set; }
+        public Guid ProdutoId { get; private set; }
+        public string ProdutoNome { get; private set; }
+        public int Quantidaade { get; private set; }
+        public decimal ValorUnitario { get; private set; }
+        public string ProdutoImagem { get; private set; }
+
+        // EF Rel.
+        public Pedido Pedido { get; private set; }
+
+        internal decimal CalcularValor() =>
+            Quantidaade * ValorUnitario;
+    }
+}
